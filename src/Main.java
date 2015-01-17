70805d3fa4ff071b57b8657ee19b29fd32949847
guaychi <guaychi@hotmail.com>
2015-01-17 21:00:14 +0100

    primerad

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Worlddddd!");
-        System.out.println("segundito");
+        System.out.println("sendito");
 
 
     }
